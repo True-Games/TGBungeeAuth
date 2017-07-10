b346db4dd9b00c8b2c052895a9a536fcf864ce07
Shevchik <Shev4ik.den@gmail.com>
2017-07-10 21:02:36 +0300

    Actually save/load sec database

--- src/tgbungeeauth/bungee/SecurityDatabase.java
+++ src/tgbungeeauth/bungee/SecurityDatabase.java
@@ -2,6 +2,7 @@ package tgbungeeauth.bungee;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashSet;
 
 import net.md_5.bungee.config.Configuration;
@@ -10,29 +11,40 @@ import net.md_5.bungee.config.YamlConfiguration;
 
 public class SecurityDatabase {
 
-	private HashSet<String> onlineMode = new HashSet<>();
+	private HashSet<String> license = new HashSet<>();
 
 	public void addOnlineMode(String name) {
-		onlineMode.add(name.toLowerCase());
+		license.add(name.toLowerCase());
 	}
 
 	public void removeOnlineMode(String name) {
-		onlineMode.remove(name.toLowerCase());
+		license.remove(name.toLowerCase());
 	}
 
 	public boolean isOnlineMode(String name) {
-		return onlineMode.contains(name.toLowerCase());
+		return license.contains(name.toLowerCase());
+	}
+
+	private File getFile() {
+		return new File(TGBungeeAuthBungee.getInstance().getDataFolder(), "database.yml");
 	}
 
 	public void load() throws IOException {
-		File db = new File(TGBungeeAuthBungee.getInstance().getDataFolder(), "database.yml");
+		File db = getFile();
 		if (!db.exists()) {
 			db.createNewFile();
 		}
 		Configuration configuration = ConfigurationProvider.getProvider(YamlConfiguration.class).load(db);
+		license.addAll(configuration.getStringList("license"));
 	}
 
 	public void save() {
+		Configuration configuration = new Configuration();
+		configuration.set("license", new ArrayList<>(license));
+		try {
+			ConfigurationProvider.getProvider(YamlConfiguration.class).save(configuration, getFile());
+		} catch (IOException e) {
+		}
 	}
 
 }
